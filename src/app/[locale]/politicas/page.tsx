45fcd7ac1d2e6cb711df24916360d230981a7c84
Fede Gilles <138924800+FedePerroAgency@users.noreply.github.com>
2026-05-22 09:50:18 -0300

    terms & privacy policy

--- src/app/[locale]/politicas/page.tsx
+++ src/app/[locale]/politicas/page.tsx
@@ -20,18 +20,7 @@ export default async function PoliticasPage({ params }: PageProps<"/[locale]/pol
 
       <main className="flex-1 p-6 sm:px-12 sm:py-16 max-w-[1000px] mx-auto">
         <h1 className="text-4xl sm:text-6xl font-thin leading-tight font-heading mb-8">{politicas.title}</h1>
-        <p className="text-foreground font-light font-body mb-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
-        <p className="text-foreground font-light font-body mb-4">
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p className="text-foreground font-light font-body mb-4">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-        </p>
-        <p className="text-foreground font-light font-body mb-4">
-          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-        </p>
+        <p className="text-foreground font-light font-body mb-4 whitespace-pre-line">{politicas.description}</p>
       </main>
 
       <SiteFooter dict={t} />
